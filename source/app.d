30b085b72c87f342cf26dbe3537055386f6078e9
CodeMyst <code@myst.rs>
2022-05-30 21:42:04 +0200

    drawing a moving rect

--- source/app.d
+++ source/app.d
@@ -9,6 +9,7 @@ enum MEM_IO   = cast(u32*) 0x04000000;
 enum MEM_VRAM = cast(u16*) 0x06000000;
 
 enum REG_DISPLAY_CONTROL = MEM_IO;
+enum REG_VCOUNT          = cast(u16*) 0x04000006;
 
 // modes
 enum DCNT_MODE3 = 0x003;
@@ -20,14 +21,14 @@ enum SCREEN_WIDTH  = 240;
 enum SCREEN_HEIGHT = 160;
 
 // colors
-enum CLR_BLACK  = 0x0000;
-enum CLR_RED    = 0x001F;
-enum CLR_LIME   = 0x03E0;
-enum CLR_YELLOW = 0x03FF;
-enum CLR_BLUE   = 0x7C00;
-enum CLR_MAG    = 0x7C1F;
-enum CLR_CYAN   = 0x7FE0;
-enum CLR_WHITE  = 0x7FFF;
+enum COL_BLACK  = 0x0000;
+enum COL_RED    = 0x001F;
+enum COL_LIME   = 0x03E0;
+enum COL_YELLOW = 0x03FF;
+enum COL_BLUE   = 0x7C00;
+enum COL_MAG    = 0x7C1F;
+enum COL_CYAN   = 0x7FE0;
+enum COL_WHITE  = 0x7FFF;
 
 u16 rgb15(u32 red, u32 green, u32 blue)
 {
@@ -39,15 +40,58 @@ void drawPixel(u32 x, u32 y, u16 color)
     MEM_VRAM[y * SCREEN_WIDTH + x] = color;
 }
 
+void drawRect(u32 left, u32 top, u32 width, u32 height, u16 color)
+{
+    for (int y = 0; y < height; y++)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            drawPixel(left + x, top + y, color);
+        }
+    }
+}
+
+/**
+ * Waits until all rows are drawn (until VBLANK). Do drawing after calling `vsync`.
+ */
+void vsync()
+{
+    while (volatileLoad(REG_VCOUNT) >= 160) {}
+    while (volatileLoad(REG_VCOUNT) < 160) {}
+}
+
 extern (C) int main()
 {
     volatileStore(REG_DISPLAY_CONTROL, DCNT_MODE3 | DCNT_BG2);
 
-    drawPixel(120, 80, rgb15(31,  0,  0));
-    drawPixel(136, 80, rgb15( 0, 31,  0));
-    drawPixel(120, 96, rgb15( 0,  0, 31));
+    // clear the screen
+    for (int y = 0; y < SCREEN_HEIGHT; y++)
+    {
+        for (int x = 0; x < SCREEN_WIDTH; x++)
+        {
+            drawPixel(x, y, COL_BLACK);
+        }
+    }
 
-    while (true) {}
+    int x = 0;
+    while (true)
+    {
+        vsync();
+
+        // if reached end of screen, go back to start
+        if (x > SCREEN_WIDTH * (SCREEN_HEIGHT / 10)) x = 0;
+
+        if (x != 0)
+        {
+            // clear the rect at the previous position
+            int previous = x - 10;
+            drawRect(previous % SCREEN_WIDTH, (previous / SCREEN_WIDTH) * 10, 10, 10, COL_BLACK);
+        }
+
+        drawRect(x % SCREEN_WIDTH, (x / SCREEN_WIDTH) * 10, 10, 10, COL_WHITE);
+
+        x += 10;
+    }
 
     return 0;
 }
